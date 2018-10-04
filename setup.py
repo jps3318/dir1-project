e0bad374c7d3570d647b89d75a4171c547ae3085
jps3318 <Joe.Stewart@Inmar.com>
2018-10-04 12:24:25 -0400

    add setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dir1",
+    version="0.0.1",
+    description="dir1 package",
+    packages=find_packages(),
+    install_requires=['requests'],
+)
